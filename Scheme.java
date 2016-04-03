96d186e6d7dd1390315a5a21b247fcf822537107
Bayless <bsmithsalzberg@stuy.edu>
2016-04-03 01:29:25 -0400

    going to sleep

--- Scheme.java
+++ Scheme.java
@@ -7,7 +7,7 @@
       2. ...
       5. Profit!
  *
- * STACK OF CHOICE: ALStack by Clyde ____
+ * STACK OF CHOICE: ALStack by BSS
  * b/c ...
  ******************************************************/
 
@@ -22,8 +22,35 @@ public class Scheme {
      *           evaluate( "( + 4 3 )" ) -> 7
      *	         evaluate( "( + 4 ( * 2 5 ) 3 )" ) -> 17
      ******************************************************/
-    public static String evaluate( String expr ) 
-    {
+    //max 2 numbers per expression
+    public static String evaluate( String expr ){
+	expr = expr(1,expr.length-1);
+	expr = expr.flip();
+	ALStack<String> storage = new ALStack<String>();
+	for (int i = 0; i<expr.length - 1; i++){
+	    if (!(expr.substring(i,i+1).equals(" "))){
+		storage.push(expr.substring(i,i+1));
+	    }
+	}
+	ALStack sub = storage;
+	int ctr = 0;
+	String result = "";
+	while (!sub.isEmpty){
+	    if (sub.peek().equals("+")){
+		sub.pop();
+		unload(1,sub);
+	    }
+	    else if (sub.peek().equals("-")){
+		sub.pop();
+		unload(2,sub);
+	    }
+	    else if (sub.peek().equals("*")){
+		sub.pop();
+		unload(3,sub);
+	    }
+	    sub.pop();
+	}
+       
     }//end evaluate()
 
 
@@ -33,12 +60,28 @@ public class Scheme {
      *           Returns the result of operating on sequence of operands.
      *           Ops: + is 1, - is 2, * is 3
      ******************************************************/
-    public static String unload( int op, Stack<String> numbers ) 
-    {
+    public static String unload( int op, Stack<String> numbers ) {
+	int first= Integer.parseInt(numbers.pop());
+	String newNum = //make back into string????
+	if (!isNumber(numbers.peek()) {
+		evaluate(newNum);
+	    }
+	if (op == 1){
+	    numbers.pop();
+	    return "" + (first + unload(1,numbers));
+	}
+	else if (op == 2){
+	    numbers.pop();
+	    return "" + (first - unload(2,numbers));
+	}
+	else if (op == 3){
+	    numbers.pop();
+	    return "" + (first * unload(1,numbers));
+	}
+	
     }//end unload()
 
 
-    /*
     //optional check-to-see-if-its-a-number helper fxn:
     public static boolean isNumber( String s ) {
         try {
@@ -49,8 +92,19 @@ public class Scheme {
 	    return false;
 	}
     }
-    */
+    public static String flip( String s ) 
+    {
+	String retStr = "";
+	ALStack<String> caleb = new ALStack<String>(s.length());
+	for (int i = 0; i < s.length(); i++){
+	    caleb.push(s.substring(i,i+1));
+	}
+	while (!(caleb.isEmpty())){
+	    retStr+=caleb.pop();
+	}
+	return retStr;
 
+    }//end flip()
 
     //main method for testing
     public static void main( String[] args ) {
